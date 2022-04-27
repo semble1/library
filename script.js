ca18823c5c339fb80a7a20fc30e56686cd098e87
semble1 <jaforkalam@gmail.com>
2022-04-26 20:08:38 -0400

    Inputting information into the form makes it pop up on the table on the page

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const pages = document.getElementById('pages');
+const finished = document.getElementById('finished');
 const popup = document.getElementById('popup-form');
 const add = document.getElementById('add');
 const submit = document.getElementById('submit');
+const closeform = document.getElementById('close');
+const table = document.getElementById('table');
 
 function showForm() {
     popup.style.display = "block";
@@ -14,3 +17,27 @@ function hideForm() {
 }
 
 add.onclick = function() {showForm()}
+
+function resetFields() {
+    title.value = ""
+    author.value = ""
+    pages.value = ""
+}
+
+closeform.onclick = function() {resetFields(); hideForm()}
+
+function insertData() {
+    let row = table.insertRow(-1);
+    let titleData = row.insertCell(0);
+    let authorData = row.insertCell(1);
+    let pagesData = row.insertCell(2);
+    let finishedData = row.insertCell(3);
+
+    titleData.innerHTML = title.value;
+    authorData.innerHTML = author.value;
+    pagesData.innerHTML = pages.value;
+    finishedData.innerHTML = `<input id="finished" name="finished" type="checkbox" ${finished.checked?"checked":""}>`
+
+}
+
+submit.onclick = function() {insertData(); resetFields(); hideForm()}
