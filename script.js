96ce3aa3677061c543f0e690f35588fffd656ed3
semble1 <jaforkalam@gmail.com>
2022-04-27 23:52:37 -0400

    Added ability to delete books once added into the library.

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const add = document.getElementById('add');
 const submit = document.getElementById('submit');
 const closeform = document.getElementById('close');
 const table = document.getElementById('table');
+let deleteRow = document.querySelectorAll('.delete');
 
 function showForm() {
     popup.style.display = "block";
@@ -32,12 +33,25 @@ function insertData() {
     let authorData = row.insertCell(1);
     let pagesData = row.insertCell(2);
     let finishedData = row.insertCell(3);
+    let deleteData = row.insertCell(4);
 
     titleData.innerHTML = title.value;
     authorData.innerHTML = author.value;
     pagesData.innerHTML = pages.value;
     finishedData.innerHTML = `<input id="finished" name="finished" type="checkbox" ${finished.checked?"checked":""}>`
+    deleteData.innerHTML = `<button class="delete">Delete</button>`
 
+    deleteRow = document.querySelectorAll('.delete')
+
+    deleteRow.forEach((r) => {
+        r.onclick = function(e) {deleteBook(e)}
+    })
 }
 
 submit.onclick = function() {insertData(); resetFields(); hideForm()}
+
+function deleteBook(e) {
+    let td = e.target.parentNode;
+    let tr = td.parentNode;
+    tr.parentNode.removeChild(tr);
+}
